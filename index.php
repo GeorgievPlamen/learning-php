e6241e6750a6d01dd1eb0a2459ee47a1396fc395
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-02-11 09:55:02 +0200

    Syntax and Variables

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Hello, World</title>
+</head>
+
+<body>
+    <h1>This is the Title</h1>
+    <?php
+
+    $name = 'Plamen';
+    echo "Hi, $name";
+
+    ?>
+</body>
+
+</html>
