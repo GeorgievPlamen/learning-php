a08aa75b22b53263ef6440ea95978245698561a7
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-02-11 10:10:35 +0200

    Conditions

---
+++ conditions.php
@@ -0,0 +1,10 @@
+<?php
+
+$first_name = "Plamen";
+$last_name = "Georgiev";
+
+if ($first_name == "Plamen" && $last_name == "Georgiev") {
+    echo "The condition is true";
+} else {
+    echo "The condition is false";
+}
