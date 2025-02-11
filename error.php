8d6805966e55a56075ea62ed14a595369e91a15a
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-02-11 10:02:28 +0200

    Error reporting

---
+++ error.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Hello, World</title>
+</head>
+
+<body>
+    <h1>This is the Title</h1>
+    <?php
+
+    $name = 'Plamen';
+    $first_name = ' ';
+    echo "Hi, $name";
+
+    echo "Hi, $first_name";
+
+    ?>
+</body>
+
+</html>
