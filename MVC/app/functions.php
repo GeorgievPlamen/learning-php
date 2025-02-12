497d02b7023b073f53652232d1f566cba9dee378
Plamen Georgiev <plamengeorgiev94dgr@gmail.com>
2025-02-12 11:05:59 +0200

    details page

--- MVC/app/functions.php
+++ MVC/app/functions.php
@@ -1,7 +1,26 @@
 <?php
 
-function view($name, $model)
+function view($name, $model = '')
 {
     global $view_bag;
     require("views/layout.view.php");
+}
+
+function redirect($url)
+{
+    header("Location: $url");
+    die();
+}
+
+function get_term($term)
+{
+    $terms = get_terms();
+
+    foreach ($terms as $item) {
+        if ($item->term == $term) {
+            return $item;
+        }
+    }
+
+    return false;
 }
